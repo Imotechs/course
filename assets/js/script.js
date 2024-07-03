61c0082eafe01f74fb47e0e74e76d189a8c9f3e9
Imotechs <adzembehj@gmail.com>
2024-07-03 11:46:29 +0100

    lesson on js

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+
+// console.log('Hello Class!!')
+// var username = 'samuel Oge'
+// var username = 'John'
+// console.log(username)
+// console.log('hello Samuel')
+// console.log(` hello ${username}`)
+
+// let myname =  "name is joy"
+// console.log(myname)
+
+// const firstName = 'Joy'
+// console.log(firstName)
+
+// const num = 2.123
+// console.log(12/num)
+// let knum;
+// console.log(knum)
+
+let person = {
+    name:'Samuel',
+    age:23,
+    DOB:'23-06-1925',
+    profile_img :'assets/images/bulb.jpg'
+}
+
+console.log(person.age)
+
+// if(firstName==='Joy'){
+//     const firstName = 'Blessing'
+// console.log(firstName)
+// }
+
+
+
+
