aad538a03aaf8e69b17ba39898e4fa5a376a494b
Imotechs <adzembehj@gmail.com>
2024-07-04 09:12:46 +0100

    added internal js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
 
-// console.log('Hello Class!!')
-// var username = 'samuel Oge'
-// var username = 'John'
-// console.log(username)
-// console.log('hello Samuel')
-// console.log(` hello ${username}`)
+console.log('Hello Class!!')
+var username = 'samuel Oge'
+var username = 'John'
+console.log(username)
+console.log('hello Samuel')
+console.log(`hello ${username}`)
 
-// let myname =  "name is joy"
-// console.log(myname)
+let myname =  "name is joy"
+console.log(myname)
 
-// const firstName = 'Joy'
-// console.log(firstName)
+const firstName = 'Joy'
+console.log(firstName)
 
-// const num = 2.123
-// console.log(12/num)
-// let knum;
-// console.log(knum)
+const num = 2.123
+console.log(12/num)
+let knum;
+console.log(knum)
 
 let person = {
     name:'Samuel',
